7fa2312fa31803fa5dc76f28ef755cc51db542dd
rcpauor32 <rcolivepau@gmail.com>
2016-02-24 22:37:53 +0100

    Added the Battleship game code

---
+++ BattleShips/Source.cpp
@@ -0,0 +1,107 @@
+#include <stdio.h>
+
+int main(){
+
+	int hit_board[10][10] = {
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	};
+
+	char board[10][10] = {
+		' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+		' ', 'A', 'C', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+		' ', ' ', 'C', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+		' ', ' ', 'C', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+		' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+		' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+		' ', ' ', ' ', ' ', ' ', 'D', 'D', 'D', 'D', ' ',
+		'B', 'B', 'B', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+		' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+		' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '
+	};
+
+	int x, y;
+
+	int All_ships = 0;
+
+	int A_ship, B_ship, C_ship, D_ship;
+	A_ship = B_ship = C_ship = D_ship = 0;
+
+	while (All_ships < 11) {
+
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (hit_board[i][j]) {
+					printf("%c ", board[i][j]);
+				}
+				else {
+					printf("* ");
+				}
+			}
+			printf("\n");
+		}
+
+		printf("\nWhere do you want to shoot? (x and y coord.): ");
+
+		scanf_s("%d %d", &x, &y);
+
+		if (hit_board[y][x]) {
+			printf("\n\nYou've already hit this tile!\n");
+		}
+		else {
+
+			hit_board[y][x] = 1;
+
+			switch (board[y][x]) {
+			case 'A': printf("\nHit!\n");
+				A_ship++;
+				All_ships++;
+				break;
+			case 'B': printf("\nHit!\n");
+				B_ship++;
+				All_ships++;
+				break;
+			case 'C': printf("\nHit!\n");
+				C_ship++;
+				All_ships++;
+				break;
+			case 'D': printf("\nHit!\n");
+				D_ship++;
+				All_ships++;
+				break;
+			case ' ': printf("\nMiss!\n");
+				break;
+			}
+
+		}
+
+		if (A_ship == 1){
+			printf("\nThe A Ship has been SLAIN!\n");
+			A_ship--;
+		}
+		if (B_ship == 2) {
+			printf("\nThe B Ship has been SLAIN!\n");
+			B_ship--;
+		}
+		if (C_ship == 3){
+			printf("\nThe C Ship has been SLAIN!\n");
+			C_ship--;
+		}
+
+	if (D_ship == 4){
+		printf("\nThe D Ship has been SLAIN!\n");
+		D_ship--;
+	}
+}											
+
+	getchar();
+	return 0;
+}
